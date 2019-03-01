d8c7e16048e8b324f8bc05edf8fde80e8ae7a948
Vilen <octopussimbiote@gmail.com>
2019-03-02 01:38:35 +0300

    added final message. Need to fix the closing

--- app/src/main/java/com/example/vil/makeeverythingok/MakeEverythingOK.java
+++ app/src/main/java/com/example/vil/makeeverythingok/MakeEverythingOK.java
@@ -2,6 +2,7 @@ package com.example.vil.makeeverythingok;
 
 import android.animation.ObjectAnimator;
 import android.animation.TimeInterpolator;
+import android.nfc.Tag;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -27,7 +28,8 @@ public class MakeEverythingOK extends AppCompatActivity {
 
     private static final TimeInterpolator ANIMATION_INTER = new DecelerateInterpolator(2);
     private static final int MAX_LEVEL = 100;
-    private static final long DURATION = 10000;
+    private static final long DURATION = 7000;
+    private static String TAG = "Tag";
 
     int progress = 0;
 
@@ -35,6 +37,8 @@ public class MakeEverythingOK extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_make_everything_ok);
+
+        Log.d(TAG, "onCreate(Bundle) called");
 
         mAlert = (TextView)findViewById(R.id.alertText);
         mProgressBar = (ProgressBar)findViewById(R.id.progressBar);
@@ -44,7 +48,8 @@ public class MakeEverythingOK extends AppCompatActivity {
             public void onClick(View v) {
                 ObjectAnimator animator = ObjectAnimator.ofInt(mProgressBar, "Progress",0,MAX_LEVEL);
 
-                mAlert.setVisibility(View.VISIBLE);
+                //mAlert.setVisibility(View.VISIBLE);
+                mAlert.setText(R.string.alert);
                 mProgressBar.setVisibility(View.VISIBLE);
                 animator.setInterpolator(ANIMATION_INTER);
                 animator.setDuration(DURATION);
@@ -55,33 +60,43 @@ public class MakeEverythingOK extends AppCompatActivity {
                     @Override
                     public void run() {
                         mAlert.setText(R.string.alertDone);
+                        mProgressBar.setVisibility(View.INVISIBLE);
+                        return;
                     }
                 }, DURATION);
 
                 }
         });
     }
-
+    
 
     @Override
     public void onStart() {
         super.onStart();
+        Log.d(TAG, "onStart() called");
     }
     @Override
     public void onPause() {
         super.onPause();
+        Log.d(TAG, "onPause() called");
+
     }
     @Override
     public void onResume() {
         super.onResume();
+        Log.d(TAG, "onResume() called");
+
     }
     @Override
     public void onStop() {
         super.onStop();
+        Log.d(TAG, "onStop() called");
     }
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+
     }
 
 }
